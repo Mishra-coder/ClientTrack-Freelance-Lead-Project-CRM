20bbdb75ffceae8e43edceb84d8c02d2f0bb4097
NssGourav <gourav341111@gmail.com>
2025-11-29 03:06:12 +0530

    creates an API helper that automatically attaches the user’s token to every request

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,17 @@
 import axios from "axios";
 
+import * as SecureStore from "expo-secure-store";
+
 const API = axios.create({
-  baseURL: "http://10.254.202.225:3000",   //your LAN IP
+  baseURL: "http://11.6.2.181:3000",   //your LAN IP
+});
+
+API.interceptors.request.use(async (config) => {
+  const token = await SecureStore.getItemAsync("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default API;
